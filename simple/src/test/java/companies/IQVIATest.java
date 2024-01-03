f22a287272738595f3ed5c1a1598f4d42f156eea
kumar <kumar@LavanyaKumar>
2024-01-03 12:20:52 +0530

    Teel them will pay this month

---
+++ simple/src/test/java/companies/IQVIATest.java
@@ -0,0 +1,11 @@
+package companies;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class IQVIATest {
+@Test
+public void Dee() {
+	Reporter.log("BBMP may visit");
+}
+}
