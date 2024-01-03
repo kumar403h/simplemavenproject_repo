fdebef1cdfccba20738cb702e9d7fbc986e822cc
kumar <kumar@LavanyaKumar>
2024-01-03 12:35:24 +0530

    Jenkins Finish

--- simple/src/test/java/Demopractice/MavenParameterTest.java
+++ simple/src/test/java/Demopractice/MavenParameterTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class MavenParameter {
+public class MavenParameterTest {
 @Test
 public void receiveData() {
 	Reporter.log("Parameter receving data",true);
@@ -14,8 +14,9 @@ public void receiveData() {
 	Reporter.log(URL,true);
 	WebDriver d = new ChromeDriver();
 	d.get(URL);
-	String name = System.getProperty("name");
-	d.findElement(By.xpath("(//input[@type='text'])[1]")).sendKeys(name);
+	Reporter.log("jenkins over",true);
+//	String name = System.getProperty("name");
+//	d.findElement(By.xpath("(//input[@type='text'])[1]")).sendKeys(name);
 	
 }
 }
